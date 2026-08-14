c427a1658f60c0342440d9d1f888a2cb4704d0e9
Aditya Rastogi <rastogiaditya301@gmail.com>
2026-08-14 15:34:40 +0530

    Update versions.tf

--- AVM/versions.tf
+++ AVM/versions.tf
@@ -1,44 +1,36 @@
 terraform {
-  required_version = "~> 1.15.0"
+  required_version = ">= 1.10.0, < 2.0.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.116.0, < 5.0.0"
     }
-
     azapi = {
       source  = "azure/azapi"
       version = ">= 2.4.0"
     }
-
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
     }
-
     random = {
       source  = "hashicorp/random"
       version = ">= 3.5.0"
     }
-
     tls = {
       source  = "hashicorp/tls"
       version = ">= 4.0.0"
     }
   }
-
-  backend "azurerm" {
-  resource_group_name  = "Aditya-RG"
-  storage_account_name = "adistoragesa"
-  container_name       = "adi-container"
-  key                  = "terraform.tfstate"
-  use_oidc             = true
-
-use_azuread_auth = true
-subscription_id  = "a587b56f-da71-4e67-b31f-8b03bc7e50ac"
-tenant_id        = "32c37a62-9306-4b6e-8c4c-ec2e9bcdc260"
-  }
+  # Temporarily commented out - backend doesn't exist yet
+  # Uncomment after creating the Azure storage account for state
+  # backend "azurerm" {
+  #   resource_group_name  = "Aditya-RG"
+  #   storage_account_name = "adistoragesa"
+  #   container_name       = "adi-container"
+  #   key                  = "terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
